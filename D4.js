12b7b96df901e40a0ddea65ec1ccb1e292e35f25
Rabz92 <rabbiailyas92@gmail.com>
2021-04-15 16:11:47 +0200

    All questions except 8

--- D4.js
+++ D4.js
@@ -73,6 +73,20 @@ Write a function "strivify" which accepts a string.
 It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
 
+const strivify = function(string){
+     
+    let firstWord = string.split(" ")[0]
+
+    if(firstWord === 'Strive'){
+        return string
+    }
+    else{
+        return `Strive ${string}`
+    }
+
+}
+
+console.log(strivify('Strive School is the best!!!'))
 
 
 /* EXERCISE 6
@@ -80,31 +94,102 @@ Write a function "check3and7" which accepts a positive number and check if it is
 HINT: Module Operator
 */
 
-/* WRITE YOUR CODE HERE */
+const check3and7 = function(positiveNumber){
+
+    let multipleOf3 =  positiveNumber % 3
+    let multipleOf7 =  positiveNumber % 7
+
+    if(multipleOf3 === 0 && multipleOf7 === 0){
+        return `${positiveNumber} is a multiple of both 3 and 7`
+    }
+    else if(multipleOf7 === 0){
+        return `${positiveNumber} is a multiple of 7`
+    }
+    else if(multipleOf3 === 0){
+        return `${positiveNumber} is a multiple of 3`
+    }
+    else{
+        return `${positiveNumber} is not a multiple of both 3 & 7`
+    }
+}
+
+console.log(check3and7(21));
+
 
 /* EXERCISE 7
 Write a function "reverseString" to programmatically reverse a given string (es.: Strive => evirtS).
 */
 
-/* WRITE YOUR CODE HERE */
+const reverseString = function(reverse){
+
+    //.split('') will split the string into an array of characters
+    //.reverse() will reverse the position of all characters
+    //.join('') will join the characters now back to string
+
+    let newString = reverse.split('').reverse().join('')
+    return newString
+}
+console.log(reverseString('Strive'));
 
 /* EXERCISE 8
 Write a function "upperFirst" to capitalize the first letter of each word of a given string passed as a parameter.
 */
 
-/* WRITE YOUR CODE HERE */
+/* const upperFirst = function(capitalize){
+    
+    //.split(" ") will split the sentence in an array or words
+    let wordArray = capitalize.split(" ")
+
+    for(let i=0; i<capitalize.length; i++){
+        
+        wordArray[i] = wordArray[i][0].toUpperCase() + wordArray[i].substr(1)
+
+        //wordArray[i][0] will give for every array of word it's first character which is at index 0 for every word. 
+        //.toUpperCase() will capitalize letter at index 0 which is first letter of each word
+        //
+    }
+    //let firstCapitalLetter = capitalize.charAt(0).toUpperCase() + capitalize.slice(1)
+    //return firstCapitalLetter
+
+   let finalWord =  wordArray.join(" ")
+   return finalWord
+}
+
+console.log(upperFirst('my name is rabia')); */
 
 /* EXERCISE 9
 Write a function "cutString" to create a new string without the first and last character of a given string.
 */
 
-/* WRITE YOUR CODE HERE */
+const cutString = function (newString) {
+
+        let lastLetter = newString.length - 1
+        newString = newString.slice(1, lastLetter)
+        return newString
+    
+}
+
+console.log(cutString('Strive'));
 
 /* EXERCISE 10
 Write a function "giveMeRandom" which accepts a number n and returns an array containing n random numbers between 0 and 10.
 */
 
-/* WRITE YOUR CODE HERE */
+const giveMeRandom = function(n){
+    
+    let randomArray = []
+
+    
+    for(let x=0; x < n; x++){
+
+    randomArray.push(Math.floor(Math.random() * 10))
+        
+    }
+
+    return randomArray
+
+}
+console.log(giveMeRandom(9));
 
 /* WHEN YOU ARE FINISHED
 Commit and push the code to your personal GitHub repository and share the link to your commit in Eduflow.
