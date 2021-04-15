a5fc3f192b74ef63cd58e50694e74e55bac89c01
Rabz92 <rabbiailyas92@gmail.com>
2021-04-15 21:38:58 +0200

    Update D4.js

--- D4.js
+++ D4.js
@@ -25,9 +25,11 @@ Write a function "crazySum" which receives two integers. It should return the su
 const crazySum = function(integer1, integer2){
     let sum = integer1 + integer2
     if(integer1 === integer2){
+        //if integer1 value is equal to integer2 value then return the value of (integer1 + inetegr2)*3
         return sum * 3
     }
     else{
+        //if integer1 is not equal to integer2 then return (inetegr1 + integer2)
         return sum
     }
 }
@@ -42,6 +44,7 @@ It should return triple their absolute difference if the given number is greater
 const crazyDiff = function(number){
     let Difference = number - 19
     let absoluteDifference = Math.abs(Difference)
+    //Math.abs() with always give us a positive value even if the result is negative i.e |2-5| = 3 not -3
     if(number > 19){
         return absoluteDifference * 3
     }
@@ -59,6 +62,7 @@ Write a function "boundary" which accept an integer n and returns true if n is w
 const boundary = function(n){
 
     if((n > 20 && n<=100) || n === 400){
+        //if integer n is a number between 20 and 100 return true. Also return true when n = 400. As in the question stated that 20 and 100 so && is an AND operator and || is an OR operator
         return true
     }
     else{
@@ -76,11 +80,18 @@ It should add the word "Strive" in front of the given string, but if the given s
 const strivify = function(string){
      
     let firstWord = string.split(" ")[0]
+    //.split(" ") will make our string of sentence into an array of words
+    //.split(" ")[0] will give us the first index of that array
+    //so our firstWord is storing an array of index 0
 
     if(firstWord === 'Strive'){
+        /* if the index[0] of array which is first word of the sentence equal to strive then write the
+        as it is */
         return string
     }
     else{
+
+      //if the first word which is index[0] of array is not Strive, Then add strive at position 0 of array
         return `Strive ${string}`
     }
 
@@ -96,10 +107,13 @@ HINT: Module Operator
 
 const check3and7 = function(positiveNumber){
 
-    let multipleOf3 =  positiveNumber % 3
+    let multipleOf3 =  positiveNumber % 3 
+    //6/3 = 0, so 6 is a multiple of 3 because the remainder is 0. So if remainder is 0 then it is
+    //multiple of 3 else it is not
     let multipleOf7 =  positiveNumber % 7
 
     if(multipleOf3 === 0 && multipleOf7 === 0){
+        //for example 21/3 and 21/7...they both give remainder 0
         return `${positiveNumber} is a multiple of both 3 and 7`
     }
     else if(multipleOf7 === 0){
@@ -143,7 +157,7 @@ Write a function "upperFirst" to capitalize the first letter of each word of a g
     for(let i=0; i<wordArray.length; i++){
         //.substr(1) will return all letter except first letter and then will combine with first letter which is not capitalized
         //charAt(0) will give character at index 0
-        //wordArray[i] will display word at index i and for each index i word, charAt(0) will display letter of that word at index 0 and .UpperCase will capitalize this word
+        //wordArray[i] will display word at index i and for each index i word, charAt(0) will display letter of that word at index 0 and .toUpperCase will capitalize this word
         wordArray[i] = wordArray[i].charAt(0).toUpperCase() + wordArray[i].substr(1)
     }
      return wordArray.join(" ")
@@ -160,6 +174,7 @@ const cutString = function (newString) {
 
         let lastLetter = newString.length - 1
         newString = newString.slice(1, lastLetter)
+        //.slice(1, lastLetter) will remove letter at index 0 and letter which is at last index
         return newString
     
 }
@@ -172,10 +187,17 @@ Write a function "giveMeRandom" which accepts a number n and returns an array co
 
 const giveMeRandom = function(n){
     
+    //declare an empty array to store values later
     let randomArray = []
 
     
     for(let x=0; x < n; x++){
+
+        //Math.random() returns floating random numbers between 0,1
+        //Multiply by 10 will give floating random numbers between 0,10
+        //Math.floor() will round the floating number to nearest integer number
+        //Math.ceil() can also be used instead of floor. It will round to nearest largest integer
+        //.push() adds elements to end of array
 
     randomArray.push(Math.floor(Math.random() * 10))
         
@@ -184,6 +206,7 @@ const giveMeRandom = function(n){
     return randomArray
 
 }
+//as n = 9, we will get 9 random numbers
 console.log(giveMeRandom(9));
 
 /* WHEN YOU ARE FINISHED
